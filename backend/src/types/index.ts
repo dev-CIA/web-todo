2acbd36eb570b362c56df172c1f139c9ce1b6570
Inae Choi <inaechoi.dev@gmail.com>
2025-03-13 01:20:56 +0900

    feat: User type 추가

--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -1,5 +1,13 @@
 import { type RowDataPacket } from "mysql2";
 
+export interface User {
+  id: number;
+  userId: string;
+  password: string;
+  salt: string;
+  createdAt: Date;
+}
+
 export interface Todo {
   id: number;
   title: string;
@@ -11,3 +19,5 @@ export interface Todo {
 }
 
 export interface TodoQueryResult extends RowDataPacket {}
+
+export interface UserQueryResult extends RowDataPacket {}
