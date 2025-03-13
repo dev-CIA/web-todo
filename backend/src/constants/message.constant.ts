82c900c18e3c539961175dc4547bd26f0b3ae2f4
Inae Choi <inaechoi.dev@gmail.com>
2025-03-13 21:41:44 +0900

    feat: 타입 추가, 성공 메시지 추가

--- backend/src/constants/message.constant.ts
+++ backend/src/constants/message.constant.ts
@@ -1,5 +1,17 @@
-export const ERROR_MESSAGES = {
-  INTERNAL_SERVER_ERROR: "서버 오류가 발생했습니다.",
-  NOT_FOUND: "리소스를 찾을 수 없습니다.",
-  UNAUTHORIZED: "인증되지 않은 사용자입니다.",
-} as const;
+import { StatusCodes } from "http-status-codes";
+
+export const ERROR_MESSAGES: Partial<Record<keyof typeof StatusCodes, string>> =
+  {
+    INTERNAL_SERVER_ERROR: "서버 오류가 발생했습니다.",
+    NOT_FOUND: "리소스를 찾을 수 없습니다.",
+    UNAUTHORIZED: "인증되지 않은 사용자입니다.",
+  } as const;
+
+export const SUCCESS_MESSAGES = {
+  REGISTER: "회원가입 성공",
+  TODO_LIST: "To-do 목록 조회 성공",
+  TODO_CREATE: "To-do 생성 성공",
+  TODO_STATUS: "To-do 상태 변경 성공",
+  TODO_EDIT: "To-do 수정 성공",
+  TODO_DELETE: "To-do 삭제 성공",
+};
